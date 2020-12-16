0b1d3287904d7a952d2d73ed9e67b19a5ec878a9
Andrew Bounds <andybounds@gmail.com>
2020-12-16 11:32:40 -0500

    Fixed accessibility of attribute

--- DSharpPlus.SlashCommands/Attributes/SlashCommandAttribute.cs
+++ DSharpPlus.SlashCommands/Attributes/SlashCommandAttribute.cs
@@ -6,7 +6,7 @@ namespace DSharpPlus.SlashCommands.Attributes
     /// Used to designate a class as a slash command.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    internal class SlashCommandAttribute : Attribute
+    public class SlashCommandAttribute : Attribute
     {
         public string Name { get; init; }
         public int Version { get; init; }
