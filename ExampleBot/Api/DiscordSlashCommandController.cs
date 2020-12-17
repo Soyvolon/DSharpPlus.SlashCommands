2f91bd8269aad3506a2acb33f5212026e4af3d77
Andrew Bounds <andybounds@gmail.com>
2020-12-17 10:56:12 -0500

    Example bot now runs

---
+++ ExampleBot/Api/DiscordSlashCommandController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.SlashCommands.Services;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ExampleBot.Api
+{
+    [Route("api/discordslash")]
+    [ApiController]
+    public class DiscordSlashCommandController : ControllerBase
+    {
+        private struct DiscordChallenge
+        {
+            [JsonProperty("type")]
+            public int type { get; set; }
+        }
+
+        [HttpPost("")]
+        //[ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> DiscordEndpointHandler()
+        {
+            JObject json;
+            try
+            {
+                var reader = new StreamReader(Request.Body);
+                if (reader.BaseStream.CanSeek)
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                var raw = await reader.ReadToEndAsync();
+                json = JObject.Parse(raw);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            if (json.ContainsKey("type") && (int)json["type"] == 1)
+                return Ok(json.ToString(Formatting.None));
+            else return Ok(); // trigger handle webhook post in DiscordSlashClient.
+        }
+    }
+}
--- ExampleBot/Program.cs
+++ ExampleBot/Program.cs
@@ -1,27 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.SlashCommands;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using Newtonsoft.Json.Linq;
+
 namespace ExampleBot
 {
     public class Program
     {
+        public static DiscordClient Discord { get; private set; }
+        public static DiscordSlashClient Slash { get; private set; }
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            MainAsync(args).GetAwaiter().GetResult();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static async Task MainAsync(string[] args)
+        {
+            // Read the config json file ...
+            using FileStream fs = new(Path.Join("Config", "bot_config.json"), FileMode.Open);
+            using StreamReader sr = new(fs);
+            var json = await sr.ReadToEndAsync();
+
+            var jobj = JObject.Parse(json);
+            // ... create a new DiscordClient for the bot ...
+            Discord = new DiscordClient(new DiscordConfiguration
+            { 
+                Token = jobj["token"].ToString(),
+                TokenType = TokenType.Bot,
+                ShardCount = 1,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+            // ... register commands ...
+            var next = Discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { jobj["prefix"].ToString() }
+            });
+
+            next.RegisterCommands(Assembly.GetExecutingAssembly());
+            // ... connect to discord ...
+            await Discord.ConnectAsync();
+            // ... use the discord connection to build the Slash Client config ...
+            Slash = new DiscordSlashClient(new DiscordSlashConfiguration
+            {
+                ClientId = Discord.CurrentApplication.Id,
+                Token = jobj["token"].ToString()
+            });
+            // ... start the slash client ...
+            await Slash.StartAsync();
+            // ... build the web server for receiving HTTP POSTs from discord ...
+            var host = Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+            // ... and start the webserver ...
+            await host.Build().StartAsync();
+            // ... then hold here to prevent premature closing.
+            await Task.Delay(-1);
+        }
     }
 }
