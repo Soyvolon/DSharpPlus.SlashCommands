6df34fad94bcbb5da8d844b9ccde828cd8b66ab3
Andrew Bounds <andybounds@gmail.com>
2020-12-17 21:03:28 -0500

    Fixed followups

--- DSharpPlus.SlashCommands/DiscordSlashClient.cs
+++ DSharpPlus.SlashCommands/DiscordSlashClient.cs
@@ -132,7 +132,7 @@ namespace DSharpPlus.SlashCommands
             var request = new HttpRequestMessage();
             request.Method = HttpMethod.Post;
             request.RequestUri = GetPostFollowupUri(token);
-            string json = JsonConvert.SerializeObject(followup, _jsonSettings);
+            string json = JsonConvert.SerializeObject(followup.BuildWebhookBody(), _jsonSettings);
             request.Content = new StringContent(json);
             request.Content.Headers.ContentType = new(_contentType);
 
--- DSharpPlus.SlashCommands/Entities/InteractionResponse.cs
+++ DSharpPlus.SlashCommands/Entities/InteractionResponse.cs
@@ -1,6 +1,9 @@
-﻿using DSharpPlus.SlashCommands.Enums;
+﻿using System;
+
+using DSharpPlus.SlashCommands.Enums;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DSharpPlus.SlashCommands.Entities
 {
@@ -10,5 +13,34 @@ namespace DSharpPlus.SlashCommands.Entities
         public InteractionResponseType Type { get; internal set; }
         [JsonProperty("data")]
         public InteractionApplicationCommandCallbackData? Data { get; internal set; }
+
+        /// <summary>
+        /// Builds the webhook body for sending a new message.
+        /// </summary>
+        /// <returns>Raw JSON body for a webhook POST operation.</returns>
+        public string BuildWebhookBody()
+        {
+            if (Data is null)
+                throw new Exception("Data can not be null.");
+
+            return JsonConvert.SerializeObject(Data); 
+        }
+
+        /// <summary>
+        /// Builds the webhook edit body for editing a previous message.
+        /// </summary>
+        /// <returns>Raw JSON body for a webhook PATCH operation.</returns>
+        public string BuildWebhookEditBody()
+        {
+            if (Data is null)
+                throw new Exception("Data can not be null.");
+
+            var d = JObject.Parse(JsonConvert.SerializeObject(Data));
+
+            if (d.ContainsKey("tts"))
+                d.Remove("tts");
+
+            return d.ToString();
+        }
     }
 }
