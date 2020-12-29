41085e972895507acad0c4a22b814f1e340a8f79
Andrew Bounds <andybounds@gmail.com>
2020-12-29 11:22:44 -0500

    Started work on parsing arguments to proper values

--- DSharpPlus.SlashCommands/Entities/SlashSubcommand.cs
+++ DSharpPlus.SlashCommands/Entities/SlashSubcommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DSharpPlus.SlashCommands.Entities
@@ -20,9 +21,48 @@ namespace DSharpPlus.SlashCommands.Entities
 
         public Task ExecuteCommand(params object[] args)
         {
-            ExecutionMethod.Invoke(BaseCommand, args);
+            var parsedArgs = ParseArguments(args);
+            ExecutionMethod.Invoke(BaseCommand, parsedArgs);
 
             return Task.CompletedTask;
+        }
+
+        private object[] ParseArguments(object[] args)
+        {
+            var parsedArgs = new object[args.Length];
+            var parameters = ExecutionMethod.GetParameters();
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                var param = parameters[i];
+                
+                if(param.ParameterType.IsEnum)
+                {
+                    var e = ParseEnum(args[i], param);
+
+                    if (e is null)
+                        throw new ArgumentNullException(param.Name, "Failed to parse Discord result to enum value.");
+
+                    parsedArgs[i] = e;
+                }
+                else
+                {
+                    parsedArgs[i] = args[i];
+                }
+            }
+
+            return parsedArgs;
+        }
+
+        private object? ParseEnum(object arg, ParameterInfo info)
+        {
+            if (int.TryParse(arg as string, out var i))
+            {
+                var e = Enum.ToObject(info.ParameterType, i);
+                return e;
+            }
+
+            return null;
         }
     }
 }
